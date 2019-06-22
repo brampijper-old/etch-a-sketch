89f99bce314ee46d2562efe68adbec52da6a4475
brampijper <brampijper@gmail.com>
2019-06-22 10:25:43 +0200

    made a 16 by 16 grid with css grid

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+const container = document.querySelector('#container');
+const amountOfDiv = 256;
+
+for(let i = 0; i < amountOfDiv; i++) {
+    let squareDiv  = document.createElement('div');
+    squareDiv.classList.add('grid-item')
+    container.appendChild(squareDiv)
+}
